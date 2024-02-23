663824f1a1a191789a962c7ad6e55cdc1144e776
sanity32 <qwe123qqqq@gmail.com>
2024-02-23 20:39:18 +0300

    1

--- loadJpeg.go
+++ loadJpeg.go
@@ -6,6 +6,6 @@ import (
 )
 
 func Load(filename string) (r Image, err error) {
-	bb, err := os.ReadFile(filename)
-	return Image(base64.StdEncoding.EncodeToString(bb)), err
+	b, err := os.ReadFile(filename)
+	return Image(base64.StdEncoding.EncodeToString(b)), err
 }
