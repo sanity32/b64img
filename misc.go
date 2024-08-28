f6ffca6b0f94a1fbbef8c6d7d47deff0ca512b36
sanity32 <qwe123qqqq@gmail.com>
2024-08-28 13:31:29 +0300

    1

--- misc.go
+++ misc.go
@@ -2,5 +2,6 @@ package b64img
 
 const (
 	PREFIX_B64_PNG = "data:image/png;base64,"
-	PREFIX_B64_JPG = "data:image/jpeg;base64,"
+	PREFIX_B64_JPG = "data:image/jpg;base64,"
+	// PREFIX_B64_JPG = "data:image/jpeg;base64,"
 )
